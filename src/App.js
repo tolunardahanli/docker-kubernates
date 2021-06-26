470baa8855aa481b432219111861a3d757513be2
tolunardahanli <38496067+tolunardahanli@users.noreply.github.com>
2021-06-26 22:19:18 +0200

    changed App Text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I updated in Feature branch.
         </p>
         <a
           className="App-link"
